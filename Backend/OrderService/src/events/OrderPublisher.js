6333e1e288a27a7a90bd67f42fec45525e2ab198
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-03-02 11:56:14 +0530

    feat: added order publisher for order service

---
+++ Backend/OrderService/src/events/OrderPublisher.js
@@ -0,0 +1,53 @@
+import { publishEvent , EXCHANGES , ROUTING_KEYS } from "./Rabbitmq.js";
+
+/**
+ * Published when a new order is created
+ * Payment Service listens to this and initiates payment
+ */
+const publishOrderCreated = async(order) => {
+    await publishEvent(EXCHANGES.ORDERS , ROUTING_KEYS.ORDER_CREATED , {
+        orderId: order._id.toString(),
+        orderNumber: order.orderNumber,
+        userId: order.userId,
+        items: order.items,
+        totalAmount: order.totalAmount,
+        currency: 'USD'
+    });
+};
+
+
+/**
+ * Published when an order is cancelled
+ * Payment Service listens and triggers refund if payment exists
+ */
+
+const publishOrderCancelled = async(order) => {
+    await publishEvent(EXCHANGES.ORDERS, ROUTING_KEYS.ORDER_CANCELLED , {
+        orderId: order._id.toString(),
+        orderNumber: order.orderNumber,
+        userId: order.userId,
+        paymentId: order.paymentId,
+        reason: 'Order cancelled by the user'
+    });
+};
+
+
+/**
+ * Published when order status changes
+ */
+const publishOrderStatusUpdate = async(order,previousStatus) => {
+    await publishEvent(EXCHANGES.ORDERS, ROUTING_KEYS.ORDER_STATUS_UPDATED , {
+        orderId: order._id.toString(),
+        orderNumber: order.orderNumber,
+        userId: order.userId,
+        previousStatus,
+        newStatus: order.status
+    })
+}
+
+
+export {
+    publishOrderCreated,
+    publishOrderCancelled,
+    publishOrderStatusUpdate
+}
