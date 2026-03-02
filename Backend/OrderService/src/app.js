9c13636853cfec5298df10c435d1fb902e9918fb
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-03-02 11:57:39 +0530

    feat: added rabbitmq to the index file

--- Backend/OrderService/src/app.js
+++ Backend/OrderService/src/app.js
@@ -6,6 +6,9 @@ import orderRoutes from './routes/orderRoutes.js';
 import internalRoutes from './routes/internalRoutes.js';
 import errorHandler from './middlewares/errorHandler.js';
 
+import {connectRabbitMQ} from './events/Rabbitmq.js'
+import {registerOrderConsumer} from './events/OrderConsumer.js'
+
 const app = express();
 
 app.use(cors({ origin: true, credentials: true }));
@@ -18,15 +21,22 @@ app.get('/health', (req, res) => res.json({ status: 'ok', service: 'order-servic
 
 app.use(errorHandler);
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
+async function startServer() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('✅ MongoDB connected');
+
+    await connectRabbitMQ();
+    await registerOrderConsumer();
+
     app.listen(process.env.PORT, () =>
       console.log(`🛒 Order Service running on port ${process.env.PORT}`)
     );
-  })
-  .catch((err) => {
-    console.error('❌ MongoDB connection error:', err.message);
+  } catch (err) {
+    console.error('❌ Bootstrap error:', err.message);
     process.exit(1);
-  });
+  }
+}
+
+
+startServer()
