b39e408431061747fd9d3140d22c350c54810281
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-03-02 11:59:17 +0530

    feat: added event publisher for the payment service

---
+++ Backend/PaymentService/src/events/PaymentPublisher.js
@@ -0,0 +1,52 @@
+import { publishEvent, EXCHANGES, ROUTING_KEYS } from './Rabbitmq.js';
+
+/**
+ * Published after payment is successfully processeds
+ * Order Service listens → confirms the order
+ */
+const publishPaymentSucceeded = async (payment) => {
+  await publishEvent(EXCHANGES.PAYMENTS, ROUTING_KEYS.PAYMENT_SUCCEEDED, {
+    paymentId: payment.paymentId,
+    orderId: payment.orderId,
+    orderNumber: payment.orderNumber,
+    userId: payment.userId,
+    amount: payment.amount,
+    currency: payment.currency,
+    transactionId: payment.gatewayTransactionId,
+  });
+};
+
+/**
+ * Published after payment fails
+ * Order Service listens → cancels the order
+ */
+const publishPaymentFailed = async (payment) => {
+  await publishEvent(EXCHANGES.PAYMENTS, ROUTING_KEYS.PAYMENT_FAILED, {
+    paymentId: payment.paymentId,
+    orderId: payment.orderId,
+    orderNumber: payment.orderNumber,
+    userId: payment.userId,
+    amount: payment.amount,
+    reason: payment.failureReason,
+  });
+};
+
+/**
+ * Published after a refund is processed
+ * Order Service listens → marks order as refunded
+ */
+const publishPaymentRefunded = async (payment) => {
+  await publishEvent(EXCHANGES.PAYMENTS, ROUTING_KEYS.PAYMENT_REFUNDED, {
+    paymentId: payment.paymentId,
+    orderId: payment.orderId,
+    orderNumber: payment.orderNumber,
+    userId: payment.userId,
+    refundAmount: payment.refundAmount,
+  });
+};
+
+export {
+  publishPaymentSucceeded,
+  publishPaymentFailed,
+  publishPaymentRefunded,
+};
